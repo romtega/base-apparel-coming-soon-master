2760e1b3da0a59791d6ed418bb55f02657ce0b6a
Roman Ortega Mondaca <romtega88@gmail.com>
2023-10-18 17:06:30 +0200

    js email validation

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log("Hello");
+
+const form = document.querySelector("#form");
+const emailInput = document.querySelector("#email-input");
+const alertIcon = document.querySelector("#alert-icon");
+const alertText = document.querySelector("#alert-text");
+const alertInput = document.querySelector("#alert-input");
+
+function validEmail() {
+  alertIcon.classList.add("hidden");
+  alertText.classList.add("hidden");
+  alertInput.classList.remove("alert-input");
+}
+
+function notValidEmail() {
+  alertIcon.classList.remove("hidden");
+  alertText.classList.remove("hidden");
+  alertInput.classList.add("alert-input");
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (emailRegex.test(emailInput.value.trim())) {
+    validEmail();
+  } else {
+    notValidEmail();
+  }
+});
